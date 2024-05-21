b3180d760e9e57df7d6d14f5e18ad189a19abe39
Jibin Kumar <jibinkumar4398@gmail.com>
2024-05-21 12:07:37 +0530

    Inbuilt authentication, later needs to work on it. session is removed in this.

--- authapp/views.py
+++ authapp/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def user_login(request):
-    if 'username' in request.session:
+    if request.user.is_authenticated:
         return redirect(home)
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user =authenticate(username=username, password=password)
         if user is not None:
-            request.session['username'] = username
+            login(request, user)
             return redirect(home)
         else:
             print('Invalid credentials')
@@ -18,12 +18,12 @@ def user_login(request):
 
 
 def home(request):
-    if 'username' in request.session:
+    if request.user.is_authenticated:
         return render(request, 'home.html')
     return redirect(user_login)
 
 
 def user_logout(request):
-    if 'username' in request.session:
-        request.session.flush()
+    if request.user.is_authenticated:
+        logout(request)
     return redirect(user_login)
